5300cb9bec7a798e1b8bebdfe5c29873c9b3c4e6
Vikings70 <119702395+Vikings70@users.noreply.github.com>
2023-01-16 00:44:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $SOURCE_CODE ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Vikings70/Channel-Mdisk-Converter
+  git clone https://github.com/Vikings70/Mdiskshort-Converter.git /Mdiskshort-Converter
 else
   echo "Cloning Custom Repo from $SOURCE_CODE "
-  git clone $SOURCE_CODE /Channel-Mdisk-Converter
+  git clone $SOURCE_CODE /Mdiskshort-Converter
 fi
-cd /Channel-Mdisk-Converter
+cd /Mdiskshort-Converter
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 main.py
